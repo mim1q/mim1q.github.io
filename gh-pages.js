e59b64ce59277d4ecaa9622333ba458d38c35ff7
mim1q <mim1q.dev@gmail.com>
2023-03-13 21:06:26 +0100

    Initial setup

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+  'build',
+  {
+    branch: 'gh-pages',
+    repo: 'https://github.com/mim1q/mim1q_personal.git',
+    user: {
+      name: 'Dominik Ziętko',
+      email: 'mim1q.dev@gmail.com',
+    },
+    dotfiles: true,
+  },
+  () => {
+    console.log('Deployed to GitHub Pages');
+  },
+).catch((error) => console.error(error));
