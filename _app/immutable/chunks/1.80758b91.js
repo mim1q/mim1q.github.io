3967dc38f14c046e28d03f9a68163345a57e9749
Dominik Ziętko <mim1q.dev@gmail.com>
2023-04-11 19:26:10 +0200

    Updates

--- _app/immutable/chunks/1.80758b91.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.64b70a2b.js";export{t as component};
---
+++ _app/immutable/chunks/2.489984f9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.cad4aff5.js";export{t as component};
